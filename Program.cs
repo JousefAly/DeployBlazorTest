1e260fe3f0652039016bc4ddac8a9189d7dba2f2
Yousef Aly <youssef.ali@arabiansystems.com>
2024-12-01 10:33:39 +0200

    use forwarded headers middleware

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using DeployBlazorTest.Components;
+using Microsoft.AspNetCore.HttpOverrides;
 using Serilog;
 
 namespace DeployBlazorTest
@@ -19,6 +20,10 @@ namespace DeployBlazorTest
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
